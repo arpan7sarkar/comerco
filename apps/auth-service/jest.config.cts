45b8b84066788407542d79a914bf60f29f4979b5
arpan <contact.arpan.sarkar@gmail.com>
2026-05-28 18:19:58 +0530

    refactor: update package names and dependencies
    - Changed package name from @comerco2/source to @comerco/source in package.json and package-lock.json.
    - Updated dependencies in package.json and package-lock.json, including axios, cookie-parser, cors, express-http-proxy, express-rate-limit, morgan, and swagger-ui-express.
    - Added new devDependencies for type definitions of cookie-parser, cors, express-http-proxy, morgan, and swagger-ui-express.
    - Introduced error handling middleware and error classes for better error management.
    - Updated tsconfig files to reflect the new package name.

--- apps/auth-service/jest.config.cts
+++ apps/auth-service/jest.config.cts
@@ -10,7 +10,7 @@ const swcJestConfig = JSON.parse(
 swcJestConfig.swcrc = false;
 
 module.exports = {
-  displayName: '@comerco2/auth-service',
+  displayName: '@comerco/auth-service',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,24 @@
-import express from 'express';
-
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+import express from "express";
+import cors from "cors";
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
 const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
-app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API'});
+app.get("/", (req, res) => {
+  res.send({ message: "Hello API" });
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+const server = app.listen(port, () => {
+  console.log(`[ ready ] http://localhost:${port}/api`);
+});
+
+server.on("error", (err) => {
+  console.error(err);
 });
